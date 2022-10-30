57e1508700e53f48b15ea4b98663fc17af86e446
Furkan Özmen <31289019+furkannzmnn@users.noreply.github.com>
2022-10-30 16:32:20 +0300

    redis commands

--- src/main/java/com/example/redisdatatypes/RedisSortedSet.java
+++ src/main/java/com/example/redisdatatypes/RedisSortedSet.java
@@ -20,21 +20,28 @@ public class RedisSortedSet {
 
     public void updateLeaderBord() {
         final String key = "leaderboard:455";
-        final String member = "furkan";
-        final Double score = 100.0;
+        final String member = "user1";
+        final int score = 100;
 
-        final String member2 = "furkan2";
-        final Double score2 = 200.0;
+        final String member2 = "user3";
+        final int score2 = 200;
 
-        final String member3 = "furkan3";
-        final Double score3 = 300.0;
+        final String member3 = "user2";
+        final int score3 = 300;
 
 
         redisTemplate.opsForZSet().add(key, member, score);
         redisTemplate.opsForZSet().add(key, member2, score2);
         redisTemplate.opsForZSet().add(key, member3, score3);
 
-        // 1. sıradaki kullanıcı
-        System.out.println("1. sıradaki kullanıcı = " + redisTemplate.opsForZSet().range(key, 0, 3));
+
+        System.out.println(redisTemplate.opsForZSet().range(key, 0, 3));
+
+        redisTemplate.opsForZSet().add(key, member3, score3 - 400);
+
+        System.out.println(redisTemplate.opsForZSet().range(key, 0, 3));
+
+
+        System.out.println(redisTemplate.opsForZSet().reverseRange(key, 0, 3));
     }
 }
